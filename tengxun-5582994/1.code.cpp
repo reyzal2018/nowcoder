41d09c799e7e799c88b3dc4b98ba15f9ab5c7f83
reyzal <reyzal@163.com>
2018-03-30 17:31:51 +0800

    修复 腾讯2017秋招笔试编程题 第一题代码

--- tengxun-5582994/1.code.cpp
+++ tengxun-5582994/1.code.cpp
@@ -1,32 +1,20 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <functional>
-#include <algorithm>
+#include <bits/stdc++.h>
  
 using namespace std;
- 
  
 int main()
 {
     string s;
-    while (cin >> s) {
-        string sr(s);
-        reverse(sr.begin(), sr.end());
-        vector<vector<int>> dp(s.length() + 1, vector<int>(s.length() + 1, 0));
-        for (int i = 1; i <= s.length(); i++)
-        {
-            for (int j = 1; j <= sr.length(); j++)
-            {
-                if (s[i-1] == sr[j-1]) {
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
-                }
-                else {
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
-                }
-            }
-        }
-        cout << s.length() - dp[s.length()][s.length()] << endl;
+    cin >> s;
+    int base[4];
+    base[3] = 1;//aaaa aaab的间距
+    base[2] = 25 * base[3] + 1;//aaa aab的间距
+    base[1] = 25 * base[2] + 1;//aa ab的间距
+    base[0] = 25 * base[1] + 1;//a b的间距
+    long long result = s.length() - 1;
+    for (int i = 0; i < s.length(); i++) {
+        result += base[i] * (s[i] - 'a');
     }
+    cout << result << endl;
     return 0;
 }
